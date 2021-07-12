18f48317e1c7f4d7d6204319890a9f85998c2661
Tarek-Abdennadher <tarek.abdennadher@aymax.fr>
2021-07-12 11:30:40 +0100

    root project

---
+++ src/test/java/fr/airfrance/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.airfrance.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

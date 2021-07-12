87cd8c53e510ca3dcb50bed4a2476cb7bdc0a6f9
Tarek-Abdennadher <tarek.abdennadher@aymax.fr>
2021-07-12 11:48:07 +0100

    add get all users and get user by login service

---
+++ src/main/java/fr/airfrance/poc/service/implementations/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package fr.airfrance.poc.service.implementations;
+
+import fr.airfrance.poc.entity.User;
+import fr.airfrance.poc.repository.UserRepository;
+import fr.airfrance.poc.service.interfaces.UserService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * <p>This method fetch all database users</p>
+     * @return
+     */
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * <p>
+     *     This method fetch all database users by pages
+     * </p>
+     * @param pageable
+     * @return
+     */
+    @Override
+    public List<User> getAll(Pageable pageable) {
+        return userRepository.getAllBy(pageable);
+    }
+
+    /**
+     * <p>
+     *     This method fetch user by login
+     * </p>
+     * @param login
+     * @return
+     */
+    @Override
+    public Optional<User> getUserByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
+}
---
+++ src/main/java/fr/airfrance/poc/service/interfaces/UserService.java
@@ -0,0 +1,17 @@
+package fr.airfrance.poc.service.interfaces;
+
+import fr.airfrance.poc.entity.User;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    List<User> getAll();
+
+    List<User> getAll(Pageable pageable);
+
+    Optional<User> getUserByLogin(String login);
+
+}
